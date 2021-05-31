4d27483476a1eb1e1c6df39d1fd5f710dc9d1ec7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2021-05-31 12:14:48 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
 pamac update --no-confirm
-pamac install jdk11-openjdk maven gradle nvm npm ttf-jetbrains-mono docker docker-compose --no-confirm
+pamac install jdk11-openjdk maven gradle nvm npm ttf-jetbrains-mono docker docker-compose snapd --no-confirm
 
 sudo usermod -aG docker $USER
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now containerd.service
+
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install insomnia
 
 echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
 source ~/.bashrc
 nvm install --lts
 
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+# mkdir ~/.npm-global
+# npm config set prefix '~/.npm-global'
+# echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
+# source ~/.bashrc
 
 npm install -g --silent @angular/cli
 
-pamac build jetbrains-toolbox google-chrome teams visual-studio-code-bin --no-confirm
+pamac build jetbrains-toolbox google-chrome teams visual-studio-code-bin insomnia --no-confirm
 
 curl https://raw.githubusercontent.com/chubbyhippo/vimrc/master/.vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/chubbyhippo/ideavimrc/main/.ideavimrc -o ~/.ideavimrc
 
 git config --global credential.helper store
-
-reboot
