ed1de252826666232d88d7a1b8be2aab1783e0b7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2021-06-01 11:56:18 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,12 +6,8 @@ echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
 source /usr/share/nvm/init-nvm.sh
 
 sudo usermod -aG docker $USER
-sudo systemctl enable --now docker.service
-sudo systemctl enable --now containerd.service
-
-# sudo systemctl enable --now snapd.socket
-# sudo ln -s /var/lib/snapd/snap /snap
-# sudo snap install insomnia
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
 curl https://raw.githubusercontent.com/chubbyhippo/vimrc/master/.vimrc -o ~/.vimrc
 curl https://raw.githubusercontent.com/chubbyhippo/ideavimrc/main/.ideavimrc -o ~/.ideavimrc
